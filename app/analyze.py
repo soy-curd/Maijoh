964f5307795c68b22c36c301491a7e355438a016
shoheitkhs <shohei@curama.jp>
2016-03-21 19:40:48 +0900

    Add tutorial.

--- app/analyze.py
+++ app/analyze.py
@@ -95,12 +95,25 @@ def log(content):
 
 
 def main():
-    x, y, d = get_data()
-    data = random.shuffle(zip(x, y))
-    train_data = data[:NUM_TESTS]
-    test_data = data[NUM_TESTS:]
+    # x, y, d = get_data()
+    def x2_plus_b(x, b):
+        _x = tf.constant(x)
+        _b = tf.constant(b)
+        result = tf.square(_x)
+        result = tf.add(result, _b)
+        return result
 
-    keep = tf.placeholder(tf.float32)
+    with tf.Session() as sess:
+        result = sess.run([x2_plus_b(2, 3)])
+        print(result)
+
+    p_x = tf.placeholder(tf.float32)
+    p_b = tf.placeholder(tf.float32)
+    p_x2_plus_b = tf.add(tf.square(p_x), p_b)
+
+    with tf.Session() as sess:
+        result = sess.run([p_x2_plus_b], feed_dict={p_x: [2], p_b: [3]})
+        print(result)
 
     """
     saver.restore(sess, "checkpoints/model-last")
