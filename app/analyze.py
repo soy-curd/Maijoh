81a52026f4d84a35159e19e1ad0f54058a9a6084
soy-curd <soycurd1@gmail.com>
2016-03-21 10:20:58 +0900

    Add analyze.py.

---
+++ app/analyze.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from download import Novel
+from constants import *
+import numpy as np
+
+
+def main():
+    print(Novel)
+
+
+if __name__ == '__main__':
+    main()
